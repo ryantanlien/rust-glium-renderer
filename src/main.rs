8d11b5eba957da9d1bde54713392f12f5463d4a6
Ryan Tan <ryantanlien99@gmail.com>
2024-09-01 21:12:11 +0800

    Initial Teapot commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use glium::Surface;
 use std::fs;
 use std::env;
+use std::io;
+use obj::load_obj;
+use obj::Obj;
 
 #[macro_use]
 extern crate glium;
@@ -13,6 +16,8 @@ struct Vertex {
   tex_coords: [f32; 2]
 }
 implement_vertex!(Vertex, position, color);
+
+//TODO: Find out some way to convert obj::vertex to Vertex struct
 
 //OpenGL refresher
 //OpenGL's coordinate system for the viewport space (aka NDC space) is a square centered at coordinate vec3(0.0, 0.0, 0.0)
@@ -27,8 +32,15 @@ fn construct_triangle_vectors() -> Vec<Vertex> {
     ]
 }
 
+// TODO: Can we use generics here to accept other formats such as &String?
 fn read_shader(shader_path: &str) -> String {
     return fs::read_to_string(std::path::Path::new(&String::from(shader_path))).unwrap()
+}
+
+// TODO: Improve error handling here by removing unwrap() and handling with ? and returning a result
+fn load_obj_file(file_path: &str) -> Obj {
+    let input = io::BufReader::new(fs::File::open(&file_path).unwrap());
+    return load_obj(input).unwrap();
 }
 
 fn create_triangle_with_colored_vertices() {
@@ -52,7 +64,6 @@ fn create_triangle_with_colored_vertices() {
     // Create empty texture
     let texture = glium::texture::Texture2d::empty(&display, 200, 200).unwrap();
     
-
     // Set Vertex Shader, ideally should be located in it's own file
     // Send matrices to vertex shader via uniforms
     // Execution is vertex shader -> fragment shader
@@ -115,8 +126,76 @@ fn create_triangle_with_colored_vertices() {
     });
 }
 
+fn create_teapot() {
+    let event_loop = glium::winit::event_loop::EventLoopBuilder::new().build().unwrap();
+    let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new().build(&event_loop);
+
+    let mut frame = display.draw();
+    frame.clear_color(0.0, 0.0, 1.0, 1.0);
+    frame.finish().unwrap();
+    
+    let shape = load_obj_file("models/obj/teapot.obj").vertices;
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+
+    // Create empty texture
+    let texture = glium::texture::Texture2d::empty(&display, 200, 200).unwrap();
+
+    let fragment_shader_src = read_shader("shaders/default.vert");
+    let vertex_shader_src = read_shader("shaders/default.frag");
+
+    let program = glium::Program::from_source(&display, vertex_shader_src.as_str(), fragment_shader_src.as_str(), None).unwrap();
+
+    let t: f32 = 0.0;
+
+    let _ = event_loop.run(move |event, window_target| {
+        match event {
+            glium::winit::event::Event::WindowEvent { event, .. } => match event {
+                glium::winit::event::WindowEvent::CloseRequested => window_target.exit(),
+                glium::winit::event::WindowEvent::Resized(window_size) => {
+                    display.resize(window_size.into());
+                },
+                glium::winit::event::WindowEvent::RedrawRequested => {
+                    // Draw code
+                    t += 0.02;
+                    
+                    // Set uniform here to be used in the shader code for animating the triangle.
+                    // The naiive approach would be to instead handle t in the event loop to update the vertex but that does not make much sense,
+                    // We can place the handling and animating of the vertexes in different positions of the animations in the shader code to push that workload to the GPU
+                    let x = t.sin() * 0.5;
+
+                    let uniforms = uniform! { 
+                        matrix: [
+                            [1.0, 0.0, 0.0, 0.0],
+                            [0.0, 1.0, 0.0, 0.0],
+                            [0.0, 0.0, 1.0, 0.0],
+                            [x, 0.0, 0.0, 1.0f32]
+                        ],
+                        tex: &texture
+                    };
+
+                    let mut target = display.draw();
+                    target.clear_color(0.0, 0.0, 1.0, 1.0);
+                    
+                    // We pass t here to the vertex shader using a uniform
+                    // A uniform is a global variable whose value is set when we draw by passing its value to the draw function.
+                    // The easiest way to do so is by using the uniform! macro
+                    target.draw(&vertex_buffer, &indices, &program, &uniforms, &Default::default()).unwrap();
+                    target.finish().unwrap();
+                }
+                _ => (),
+            },
+            glium::winit::event::Event::AboutToWait => {
+                window.request_redraw();
+            }
+            _ => (),
+        };
+    }); 
+}
+
 // Note: Remember that matrices in OpenGL are in column-major order
 fn main() {
-    create_triangle_with_colored_vertices()
+    // create_triangle_with_colored_vertices()
+    create_teapot();
 }
 
