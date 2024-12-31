b1f390a7aac4fcf2ca57dd5eef10d591a57628a1
Ryan Tan <ryantanlien99@gmail.com>
2025-01-01 02:19:34 +0800

    Record keystrokes to rotate around y axis with A and D

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,7 @@ impl From<obj::Vertex> for Vertex {
     }
 }
 
+// Equivalent to OpenGL gluLookAt
 fn view_matrix(position: &[f32; 3], direction: &[f32;3], up: &[f32; 3]) -> [[f32;4]; 4] {
     let f = {
         let f = direction;
@@ -74,10 +75,28 @@ fn load_obj_file(file_path: &str) -> Obj {
     return load_obj(input).unwrap();
 }
 
+fn distance(p1: &[f32; 3], p2: &[f32; 3]) -> f32 {
+    let term1 = p1[0] - p2[0];
+    let term2 = p1[1] - p2[1];
+    let term3 = p1[2] - p2[2];
+    return f32::sqrt(term1 * term1 + term2 * term2 + term3 * term3);
+}
+
+fn magnitude(p: &[f32; 3]) -> f32 {
+    return f32::sqrt(p[0] * p[0] + p[1] * p[1] + p[2] * p[2]);
+} 
+
+fn update_lookat_direction(camera_position: &[f32; 3], lookat_position: &[f32; 3]) -> [f32; 3] {
+    let term1 = lookat_position[0] - camera_position[0];
+    let term2 = lookat_position[1] - camera_position[1];
+    let term3 = lookat_position[2] - camera_position[2];
+    let vector = [term1, term2, term3];
+    return [term1 / magnitude(&vector), term2 / magnitude(&vector), term3 / magnitude(&vector)];
+}
+
 fn create_teapot() {
     let event_loop = glium::winit::event_loop::EventLoopBuilder::new().build().unwrap();
     let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new().build(&event_loop);
-
     
     let obj_file = load_obj_file("models/obj/teapot.obj");
     
@@ -101,10 +120,58 @@ fn create_teapot() {
 
     let light = [-1.0, 0.4, 0.9f32];
 
+    // TODO: Dynamically edit this view matrix to allow for camera movement with WASD
+    // TODO: Need some way to record keystrokes and update the view matrix accordingly
+    // Note: Teapot bottom is located at approximately [0.0, -0.5,  2.0]
+
+    // To rotate around the lookAt point, need to change the up vector and the position of the camera.
+    // For example, we "rotate" by theta angles around the x axis in a quarternion
+    // Then we need to shift the z and y coordinates using simple trigonometry
+    
+    let teapot_position = [0.0, -0.5, 2.0];
+
+    let mut camera_position = [0.0, 0.5, 0.0];
+    let mut camera_direction = [teapot_position[0] - camera_position[0], teapot_position[1] - camera_position[1], teapot_position[2] - camera_position[2]];
+    let camera_up = [0.0, 1.0, 0.0];
+    let mut view = view_matrix(&camera_position, &camera_direction, &camera_up);
+    // let view = view_matrix(&[0.0, 3.0, 2.0], &[0.0, -1.0, 0.0], &[0.0, 0.0, 1.0]);
+    
+    let radius = distance(&camera_position, &teapot_position);
+    //simple rotation around the y axis
+    let mut x_angle = 0.0;
+    let mut z_angle = 0.0;
 
     let _ = event_loop.run(move |event, window_target| {
         match event {
             glium::winit::event::Event::WindowEvent { event, .. } => match event {
+                glium::winit::event::WindowEvent::KeyboardInput { device_id, event, is_synthetic } => match event {
+                    // println!("Keyboard input event: {:?}", event);
+                    glium::winit::event::KeyEvent { physical_key, .. } => match physical_key {
+                        glium::winit::keyboard::PhysicalKey::Code(key_code) => match key_code {
+                            glium::winit::keyboard::KeyCode::KeyW => println!("Key: {:?}", key_code),
+                            glium::winit::keyboard::KeyCode::KeyA => {
+                                x_angle -= 0.05;
+                                z_angle -= 0.05;
+                                camera_position = [teapot_position[0] + f32::sin(x_angle) * radius, 0.5, teapot_position[2] + f32::cos(z_angle) * radius];
+                                camera_direction = update_lookat_direction(&camera_position, &teapot_position);
+                                println!("Camera position: {:?}", camera_position);
+                                view = view_matrix(&camera_position, &camera_direction, &camera_up); 
+                            }
+                            glium::winit::keyboard::KeyCode::KeyS => println!("Key: {:?}", key_code),
+                            glium::winit::keyboard::KeyCode::KeyD => {
+                                x_angle -= 0.05;
+                                z_angle -= 0.05;
+                                camera_position = [teapot_position[0] + f32::cos(x_angle) * radius, 0.5, teapot_position[2] + f32::sin(z_angle) * radius];
+                                camera_direction = update_lookat_direction(&camera_position, &teapot_position);
+                                println!("Camera position: {:?}", camera_position);
+                                view = view_matrix(&camera_position, &camera_direction, &camera_up); 
+                            },
+                            _ => ()
+                        }, 
+                        _ => ()
+                    },
+                    _ => ()
+                },
                 glium::winit::event::WindowEvent::CloseRequested => window_target.exit(),
                 glium::winit::event::WindowEvent::Resized(window_size) => {
                     display.resize(window_size.into());
@@ -131,25 +198,24 @@ fn create_teapot() {
                         ]
                     };
 
-                    let view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
-                    
                     // Set uniform here to be used in the shader code for animating the triangle.
                     // The naiive approach would be to instead handle t in the event loop to update the vertex but that does not make much sense,
                     // We can place the handling and animating of the vertexes in different positions of the animations in the shader code to push that workload to the GPU
                     let x = 0.0;
 
-                    // Remember that in CG most matrices are in column-major order
-                    // So matrix is actually 
-                    // 0.05 0.0 0.0 x
-                    // 0.0 0.05 0.0 0.0
-                    // 0.0 0.0 0.05 0.0
-                    // 0.0 0.0 2.0 1.0
-                    // in row major order
-
                     // In column major order, order of transformations is inverse that of multiplication
                     // So for transform: scale, rotate then translate, the order of multiplication is translate * rotate * scale * vector
                     // In row major order, the order of multiplication is scale * rotate * translate * vector
                     let uniforms = uniform! { 
+                        // Remember that in CG most matrices are in column-major order
+                        // So matrix is actually 
+                        // 0.05 0.0 0.0 x
+                        // 0.0 0.05 0.0 0.0
+                        // 0.0 0.0 0.05 0.0
+                        // 0.0 0.0 2.0 1.0
+                        // in row major order
+
+                        // This model matrix is just a scale
                         model: [
                             [0.05, 0.0, 0.0, 0.0],
                             [0.0, 0.05, 0.0, 0.0],
